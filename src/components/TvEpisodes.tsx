5998a0f524497f4bd2733d2542f60cc42d928df1
eduardo239 <eucrieiumaconta@gmail.com>
2023-11-10 09:17:40 -0300

    ajustes no sistema de series

--- src/components/TvEpisodes.tsx
+++ src/components/TvEpisodes.tsx
@@ -1,43 +1,65 @@
 import { useState } from "react";
-import { ITrackerTv, ITvDetails } from "../abstract/interfaces";
+import { ITvDetails } from "../abstract/interfaces";
 import { useAuth } from "../context/AuthContext";
-import { Button, Grid, Header, Segment } from "semantic-ui-react";
+import { Button, Header, Segment } from "semantic-ui-react";
+
+interface IEpisode {
+  season_number: number;
+  episodes: number[];
+}
+
+interface ITracker {
+  data: IEpisode[];
+}
+
+const tracker_ = {
+  data: [],
+};
 
 const TvEpisodes = ({ data }: { data: ITvDetails }) => {
   const { user } = useAuth();
 
-  const [tvTracker, setTvTracker] = useState<[]>([]);
+  const [tvTracker, setTvTracker] = useState<ITracker>(tracker_);
 
   function addItemToArrayIfNotExists(season: number, episode: number) {
-    console.log(season, episode);
+    // console.log(season, episode);
+    const _sExists = tvTracker.data.some((x) => x.season_number === season);
 
     // check if season exists
 
-    // // if not exists add new season and ep
+    if (!_sExists) {
+      const data_ = tvTracker;
+      const content_ = {
+        season_number: season,
+        episodes: [episode],
+      };
 
-    // // if exists update ep
+      // added new data
+      data_.data.push(content_);
 
-    const t = {
-      data: {
-        seasons: [
-          {
-            season: season,
-            episodes: [episode],
-          },
-        ],
-      },
-    };
+      setTvTracker(data_);
+    } else {
+      const data_ = tvTracker;
+      const index = data_.data.findIndex((o) => {
+        return o.season_number === season;
+      });
 
-    console.log(t);
-
-    // const a = seasons.filter((e) => e.episode_number !== episode);
-    return true;
+      // add/remove episode
+      const _eExists = data_.data[index].episodes.some((x) => x === episode);
+      if (_eExists) {
+        console.log("remove episode");
+        const newArray = data_.data[index].episodes.filter(
+          (x) => x !== episode
+        );
+        data_.data[index].episodes = newArray;
+        setTvTracker(data_);
+      } else {
+        data_.data[index].episodes.push(episode);
+        setTvTracker(data_);
+      }
+    }
   }
-
-  // const _ep = { episode_number: ep.episode_number, watched: ep.watched };
-  // const _se = [_ep];
-  // const _tr = { data: [{ episodes: _se, season_number: 1 }] };
-
+  console.log(tvTracker);
   return (
     <Segment basic>
       {data.seasons.map((season, indexSeason) => (
@@ -46,13 +68,22 @@ const TvEpisodes = ({ data }: { data: ITvDetails }) => {
           <div className="flex">
             {[...Array(season.episode_count).keys()].map((ep, indexEpisode) => (
               <Button
+                className={`${
+                  tvTracker && tvTracker.data && tvTracker.data[indexSeason]
+                    ? tvTracker.data[indexSeason].episodes.some(
+                        (x) => x === indexEpisode + 1
+                      )
+                      ? "x"
+                      : ""
+                    : "x"
+                }`}
                 key={indexEpisode}
                 style={{ margin: 0 }}
                 onClick={() =>
                   addItemToArrayIfNotExists(indexSeason + 1, indexEpisode + 1)
                 }
               >
-                EP {ep + 1}
+                EP {ep + 1 < 10 ? "0" + (ep + 1) : ep + 1}
               </Button>
             ))}
           </div>
