7a65566910cebde9cea48526d1e64791eac7816d
eduardo239 <eucrieiumaconta@gmail.com>
2023-12-08 17:10:02 -0300

    adicionado opcao para ver mais do elenco

--- src/components/DataCast.tsx
+++ src/components/DataCast.tsx
@@ -1,21 +1,33 @@
-import { Header } from "semantic-ui-react";
+import { Button } from "semantic-ui-react";
 import { ICast } from "../abstract/interfaces";
 import GridContainer from "./GridContainer";
 import PersonGroup from "./PersonGroup";
 import TitleInfo from "./TitleInfo";
+import { useState } from "react";
 
 type TDataCast = {
   data: { credits: { cast: ICast[] } };
 };
 
 const DataCast = ({ data }: TDataCast) => {
+  const [length, setLength] = useState(5);
+
+  const handleChangeLength = (_length: number) => {
+    if (length + _length <= 4) {
+      return;
+    }
+    setLength(length + _length);
+  };
+
   if (data.credits.cast.length > 0)
     return (
       <>
         <TitleInfo center title="Elenco" />
         <GridContainer centered gap="gap-sm">
-          <PersonGroup data={data} />
+          <PersonGroup data={data} length={length} />
         </GridContainer>
+        <Button onClick={() => handleChangeLength(5)}>Ver Mais</Button>
+        <Button onClick={() => handleChangeLength(-5)}>Ver Menos</Button>
       </>
     );
 };
--- src/components/PersonGroup.tsx
+++ src/components/PersonGroup.tsx
@@ -2,13 +2,13 @@ import { ICast } from "../abstract/interfaces";
 import CastProfile from "./CastProfile";
 import MessageNotFound from "./MessageNotFound";
 
-type TPersonGroup = { data: { credits: { cast: ICast[] } } };
+type TPersonGroup = { data: { credits: { cast: ICast[] } }; length?: number };
 
-const PersonGroup = ({ data }: TPersonGroup) => {
+const PersonGroup = ({ data, length = 5 }: TPersonGroup) => {
   return data.credits && data.credits.cast.length > 0 ? (
     data.credits.cast
       .map((a) => <CastProfile key={a.id} cast={a} />)
-      .slice(0, 5)
+      .slice(0, length)
   ) : (
     <MessageNotFound message="Elenco não encontrado" />
   );
